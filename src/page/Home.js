cb628d3eebc6d2cc68c46b442d8e3216ff42a7ad
Aybergen <ai_bolat@kbtu.kz>
2023-10-28 15:08:57 +0600

    hi

---
+++ src/page/Home.js
@@ -0,0 +1,30 @@
+// src/components/HomePage.js
+import React, { useState } from 'react';
+import Post from '../post/Post';
+
+const HomePage = () => {
+    const [posts, setPosts] = useState([
+        { postId: 1, title: 'Post 1' },
+        { postId: 2, title: 'Post 2' },
+    ]);
+
+    const handleDeletePost = (postId) => {
+        setPosts(posts.filter(post => post.postId !== postId));
+    };
+
+    return (
+        <div className="home-page">
+            <h1>Welcome to the Home Page</h1>
+            {posts.map(post => (
+                <Post
+                    key={post.postId}
+                    postId={post.postId}
+                    title={post.title}
+                    onDelete={handleDeletePost}
+                />
+            ))}
+        </div>
+    );
+};
+
+export default HomePage;
---
+++ src/page/Profile.js
@@ -0,0 +1,106 @@
+import React, { useState } from 'react';
+
+const Profile = () => {
+    const [userInfo, setUserInfo] = useState({
+        name: 'John',
+        surname: 'Doe',
+        birthday: '1990-01-01',
+        email: 'john.doe@example.com',
+        password: '********',
+        profilePicture: null
+    });
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setUserInfo({
+            ...userInfo,
+            [name]: value
+        });
+    };
+
+    const handleFileInputChange = (e) => {
+        const file = e.target.files[0];
+        setUserInfo({
+            ...userInfo,
+            profilePicture: file
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Add code here to handle form submission, including profile picture upload
+        console.log(userInfo);
+    };
+
+    return (
+        <div>
+            <h1>Profile Page</h1>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="name">Name:</label>
+                    <input
+                        type="text"
+                        id="name"
+                        name="name"
+                        value={userInfo.name}
+                        onChange={handleInputChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="surname">Surname:</label>
+                    <input
+                        type="text"
+                        id="surname"
+                        name="surname"
+                        value={userInfo.surname}
+                        onChange={handleInputChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="birthday">Birthday:</label>
+                    <input
+                        type="date"
+                        id="birthday"
+                        name="birthday"
+                        value={userInfo.birthday}
+                        onChange={handleInputChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="email">Email:</label>
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        value={userInfo.email}
+                        onChange={handleInputChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Password:</label>
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        value={userInfo.password}
+                        onChange={handleInputChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="profilePicture">Profile Picture:</label>
+                    <input
+                        type="file"
+                        id="profilePicture"
+                        name="profilePicture"
+                        onChange={handleFileInputChange}
+                    />
+                </div>
+                <div>
+                    <button type="submit">Save Changes</button>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default Profile;
